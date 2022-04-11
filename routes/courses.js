215a304a501f956bf01b0ff7e6e0b200270a138c
Volodimir Stasiuk <vovastasyuk262@ukr.net>
2022-04-11 23:14:00 +0300

    release v1

---
+++ routes/courses.js
@@ -0,0 +1,14 @@
+const {Router} = require('express')
+const Course = require('../models/course')
+const router = Router()
+
+router.get('/', async (req, res) => {
+  const courses = await Course.getAll()
+  res.render('courses', {
+    title: 'Курсы',
+    isCourses: true,
+    courses
+  })
+})
+
+module.exports = router
